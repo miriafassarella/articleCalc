9593f3fcf7150a4984a6934b8bd80cb4d70c1b53
Miriã Fassarella <miriafassarella@gmail.com>
2019-07-09 16:47:40 -0300

    recebendo evetos de click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,23 +4,33 @@ constructor(){
     /*this transforma uma variável em um atributo..
     faz referência ao objeto que foi instanciado a classe.*/
     this._displayCalcEl = document.querySelector("#display");//aqui nós amarramos nosso elemento ao javascript.
-    this.initialize(); //colocamos aqui pq o construtor é iniciado altomaticamente.
-    this.initButtonsEvents();
+    this.initial(); //colocamos aqui pq o construtor é iniciado automaticamente.
+    this.buttonsEvents();
 }
 
-initialize(){//quando a calculadora inicia.
+initial(){//quando a calculadora inicia.
     
  
 }
-initButtonsEvents(){
 
-    let buttons = document.querySelectorAll("#row-buttons > .button");//Amarrando a cada elemento.
-    buttons.forEach(button=>{//percorrendo cada elemento.
-        button.addEventListener('click', e=>{//adicionando o evento.
-            console.log(e);
+
+buttonsEvents(){
+
+    let buttons = document.querySelectorAll("#row-buttons > div");//Amarrando a cada elemento.
+
+    buttons.forEach(btn=>{//percorrendo cada elemento.
+        //recebendo o resultado no click no console.
+        btn.addEventListener("click", e=>{
+            console.log(btn.className.replace("button", ""));
+        })
+        //recebendo o resultado de passar o mouse em cima.
+        btn.addEventListener("mouseover", e=>{
+            btn.style.cursor = "pointer";
         })
 
-    })
+        })
+
+
         
     
 }
